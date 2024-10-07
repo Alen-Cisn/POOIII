a5ec2cef2a0b8981cf254192ee65f688b980d145
MrtSoria <martin_soria02@outlook.com>
2024-10-07 18:51:36 -0300

    TCP Client with Forms

---
+++ ClienteTCP/ClienteTCP/Form2.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ClienteTCP
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(24, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 30);
+            label1.TabIndex = 0;
+            label1.Text = "ID: ";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(150, 21);
+            button2.Name = "button2";
+            button2.Size = new Size(45, 32);
+            button2.TabIndex = 8;
+            button2.Text = "Copy";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.MenuText;
+            textBox1.Font = new Font("Consolas", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = SystemColors.Window;
+            textBox1.Location = new Point(24, 361);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(470, 22);
+            textBox1.TabIndex = 9;
+            textBox1.KeyDown += textBox1_KeyDown;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = SystemColors.MenuText;
+            richTextBox1.Font = new Font("Consolas", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            richTextBox1.ForeColor = SystemColors.Window;
+            richTextBox1.Location = new Point(24, 59);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(470, 293);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.TabStop = false;
+            richTextBox1.Text = "";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(26, 26, 26);
+            ClientSize = new Size(513, 399);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(richTextBox1);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ ClienteTCP/ClienteTCP/Form2.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteTCP
+{
+    public partial class Form2 : Form
+    {
+        private TcpClient tcpClient;
+        private NetworkStream stream;
+        private int puertoLocal;
+
+        public Form2(string ip, int port)
+        {
+            InitializeComponent();
+            InitializeConection(ip, port);
+
+            this.FormClosing += new FormClosingEventHandler(Form2_FormClosing);
+        }
+
+        private void InitializeConection(string ip, int port)
+        {
+            try
+            {
+                tcpClient = new TcpClient(ip, port);
+                stream = tcpClient.GetStream();
+                puertoLocal = ((System.Net.IPEndPoint)tcpClient.Client.LocalEndPoint).Port;
+                label1.Text = "ID: " + puertoLocal;
+
+                Task.Run(() => recibirMensajes());
+            }
+            catch (Exception ex){ 
+                MessageBox.Show("Error: " + ex.Message );
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                EnviarMensaje();
+            }
+        }
+
+        private void EnviarMensaje()
+        {
+            try
+            {
+                string mensaje = textBox1.Text.Trim();
+                if (!string.IsNullOrWhiteSpace(mensaje))
+                {
+                    richTextBox1.AppendText("C>: " + mensaje + Environment.NewLine);
+
+                    byte[] datos = Encoding.ASCII.GetBytes(mensaje + Environment.NewLine);
+                    stream.Write(datos, 0, datos.Length);
+
+                    textBox1.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async void recibirMensajes()
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string mensajeRecibido = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                    richTextBox1.Invoke((MethodInvoker)delegate
+                    {
+                        richTextBox1.AppendText("S>: " + mensajeRecibido + Environment.NewLine);
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(puertoLocal.ToString());
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (tcpClient != null)
+            {
+                stream.Close();
+                tcpClient.Close();
+                Application.Exit();
+            }
+        }
+    }
+}
